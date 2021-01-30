0bfd66a302180ccc858daf35bc4923fe5aed11b3
tomas <cmg1411@naver.com>
2021-01-31 00:06:58 +0900

    feat: @Order 실습

--- src/test/java/com/whiteship/white_ship_study/classExample/CarTest.java
+++ src/test/java/com/whiteship/white_ship_study/classExample/CarTest.java
@@ -1,7 +1,29 @@
 package com.whiteship.white_ship_study.classExample;
 
+import org.junit.jupiter.api.*;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_METHOD)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class CarTest {
-    
+    private int number = 0;
+
+    @Order(3)
+    @Test
+    void test1() {
+        System.out.println(1);
+    }
+
+    @Order(2)
+    @Test
+    void test2() {
+        System.out.println(2);
+    }
+
+    @Order(1)
+    @Test
+    void test3() {
+        System.out.println(3);
+    }
 }
